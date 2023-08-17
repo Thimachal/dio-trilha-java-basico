1255a21c23938081680c3acb0344ee057157d248
Thimachal <thimachal@gmail.com>
2023-08-17 17:31:26 -0300

    exercicio concluido

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -9,16 +9,16 @@ public class ContaTerminal {
         String name;
         Double balanceCount;
 
-        System.out.println("Por favor, digite ");
-
-        System.out.println();
-
-        System.out.println();
-
-        System.out.println();
-
-        System.out.println("Olá " + name + "ghg");
-
+        System.out.println("Por favor, digite o número da Agência !");
+        agency = scanner.nextLine();
+        System.out.println("Por favor, digite o número da Conta !");
+        numberCount = scanner.nextInt();
+        System.out.println("Por favor, digite o seu nome !");
+        name = scanner.next();
+        System.out.println("Por favor, digite o seu saldo !");
+        balanceCount = scanner.nextDouble();
+        System.out.println("Olá " + name + ", obrigado por criar uma conta em nosso banco, sua agência é " + agency +
+                ", conta " + numberCount + " e seu saldo " + balanceCount + " já está disponível para saque");
 
     }
 }
