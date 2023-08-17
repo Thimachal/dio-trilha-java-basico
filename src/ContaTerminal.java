e9c03866a64cd86b435d2d819bbb6ce25cdae0d2
Thimachal <thimachal@gmail.com>
2023-08-17 16:53:36 -0300

    first commit

---
+++ src/ContaTerminal.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Integer numberCount;
+        String agency;
+        String name;
+        Double balanceCount;
+
+        System.out.println("Por favor, digite ");
+
+        System.out.println();
+
+        System.out.println();
+
+        System.out.println();
+
+        System.out.println("Olá " + name + "ghg");
+
+
+    }
+}
