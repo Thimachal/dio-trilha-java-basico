8e476daeb2c1b612e0c5e74ffe68d57354778522
Thimachal <thimachal@gmail.com>
2023-08-20 10:03:03 -0300

    trycatch

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ContaTerminal {
@@ -9,18 +10,22 @@ public class ContaTerminal {
         String name;
         Double balanceCount;
 
-        System.out.println("Por favor, digite o número da Agência !");
-        agency = scanner.nextLine();
-        System.out.println("Por favor, digite o número da Conta !");
-        numberCount = scanner.nextInt();
-        System.out.println("Por favor, digite o seu nome !");
-        name = scanner.next();
-        System.out.println("Por favor, digite o seu saldo !");
-        balanceCount = scanner.nextDouble();
-        System.out.println("Olá " + name + ", obrigado por criar uma conta em nosso banco, sua agência é " + agency +
-                ", conta " + numberCount + " e seu saldo " + balanceCount + " já está disponível para saque");
+        try {
+            System.out.println("Por favor, digite o número da Agência !");
+            agency = scanner.nextLine();
+            System.out.println("Por favor, digite o número da Conta !");
+            numberCount = scanner.nextInt();
+            System.out.println("Por favor, digite o seu nome !");
+            name = scanner.next();
+            System.out.println("Por favor, digite o seu saldo !");
+            balanceCount = scanner.nextDouble();
+            System.out.println("Olá " + name + ", obrigado por criar uma conta em nosso banco, sua agência é " + agency +
+                    ", conta " + numberCount + " e seu saldo " + balanceCount + " já está disponível para saque");
 
-                scanner.close();
+            scanner.close();
 
+        }catch(InputMismatchException exception){
+            System.err.println("O saldo precisa ser númerico, favor tente novamente");
+        }
     }
 }
